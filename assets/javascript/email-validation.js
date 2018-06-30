6eac941dbcb6d9d43e19cbaec4cacbc4c85d64c7
tunjiakanbi <tunjiakanbi@gmail.com>
2018-06-30 01:23:57 -0500

    tunjiProj1

---
+++ assets/javascript/email-validation.js
@@ -0,0 +1,17 @@
+var inputText = $("#txtEmail").val;
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+document.form1.text1.focus();
+return true;
+console.log("you are logged in");
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
