4f0fda678c8b024c090693e338ca301baa80afb1
Kennet Bitzer <bitze003@d.umn.edu>
2018-06-30 01:27:16 -0500

    almostFinished

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -13,12 +13,14 @@
 
     function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 12,
+            zoom: 14,
             center: {
                 lat: 44.9375,
                 lng: -93.2010
             }
+            
         });
+        
 
         var geocoder = new google.maps.Geocoder();
 
@@ -34,7 +36,7 @@
                 geocodeAddress(geocoder, map, temp[i].EventTitle, temp[i].EventLocation, temp[i].EventTime, temp[i].EventDescription)
             }
         })
-
+//Takes address from firebase and pins it to the map
         function geocodeAddress(geocoder, resultsMap, EventTitle, EventLocation, EventTime, EventDescription) {
             geocoder.geocode({
                 'address': EventLocation,
@@ -43,17 +45,56 @@
                     resultsMap.setCenter(results[0].geometry.location);
                     var marker = new google.maps.Marker({
                         map: resultsMap,
-                        position: results[0].geometry.location
+                        position: results[0].geometry.location,
+                        icon: href="assets/images/blue-marker.png"
                     });
                 } else {
                     alert('Geocode was not successful for the following reason: ' + status);
                 }
+                // adds text box to the map when clicked on
                 var infoWindow = new google.maps.InfoWindow({
                     content: '<p>' + EventTitle + '<p>' +'<p>' + EventLocation + '<p>' +'<p>' + EventTime + '<p>' +'<p>' + EventDescription + '<p>',
                 });
+                //geolocates your location if it is agreed in browser
                 marker.addListener('click', function () {
                     infoWindow.open(map, marker);
                 });
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                      var pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                      };
+                      //adds green pin to your location
+                      
+                var Geolocate = new google.maps.InfoWindow({
+
+                })
+                      Geolocate.setPosition(pos);
+                      Geolocate.setContent('You are Here');
+                      Geolocate.open(map);
+                      map.setCenter(pos);
+                      var yourLocation = new google.maps.Marker({
+                        map: resultsMap,
+                        position: pos,
+                        icon: href="assets/images/free-3-green.png"
+                    });
+                    var yourLocation = new google.maps.Marker({
+
+                                       
+                        
+                    });
+                    yourLocation.addListener('click', function () {
+                        infoWindow.open(map, yourLocation);
+                    });
+                    }, function() {
+                      handleLocationError(true, Geolocate, map.getCenter());
+                    });
+                  } else {
+                    // Browser doesn't support Geolocation
+                    handleLocationError(false, Geolocate, map.getCenter());
+                  }
             });
+            
         }
     }
